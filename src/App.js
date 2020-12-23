7d62ffaf4dfcb91422f8113360ad911f674c34fc
samuelrrs <samuellrrs@gmail.com>
2020-12-23 10:41:30 -0300

    fix: removido container em volta do container principal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css'
 import ContainerPrincipal from './containers/ContainerPrincipal/index';
-import { Container } from '@material-ui/core';
 function App() {
   return (
-    <Container>
       <ContainerPrincipal/> 
-    </Container>
   );
 }
 
