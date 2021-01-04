9bdd04fe4992aeb6e2f637f1f116e82a6071a7f0
samuelrrs <samuellrrs@gmail.com>
2021-01-04 17:13:11 -0300

    feat: estilo do card com makeStyles do material

---
+++ src/Components/CardTask/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  cardPrincipal: {
+    backgroundColor: "#565B82",
+    color: "#ffff"
+  },
+}));
+
+export default useStyles;
