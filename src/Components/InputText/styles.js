0785555123afbb25cc5176a4670b3b51ee4e03b8
samuelrrs <samuellrrs@gmail.com>
2021-01-05 10:38:48 -0300

    refactor: novo estilo do input, cores foram padronizadas conforme o estilo do programa

--- src/Components/InputText/styles.js
+++ src/Components/InputText/styles.js
@@ -3,24 +3,33 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(() => ({
   input: {
     width: "200px",
-    color: "#fff !important",
-    "& .MuiInputBase-input": {
-      color: "#839BB7",
-    },
-    "& fieldset": {
+    color: "white",
+    "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
       borderColor: "white",
     },
-    "&:hover fieldset": {
-      borderColor: "red",
+    "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+      borderColor: "white",
     },
-    "& label": {
+    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      borderColor: "gray",
+    },
+    "& .MuiOutlinedInput-input": {
       color: "gray",
     },
-    "& label.Mui-focused": {
-      color: "#839BB7",
+    "&:hover .MuiOutlinedInput-input": {
+      color: "gray",
     },
-    "&:hover .MuiOutlinedInput-notchedOutline": {
-      color: "red"
+    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input": {
+      color: "gray",
+    },
+    "& .MuiInputLabel-outlined": {
+      color: "gray",
+    },
+    "&:hover .MuiInputLabel-outlined": {
+      color: "gray",
+    },
+    "& .MuiInputLabel-outlined.Mui-focused": {
+      color: "white",
     },
   },
 }));
