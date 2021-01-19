db5ea4cb7d4ed397b5c7d2a6c22e7689da2a51dd
samuelrrs <samuellrrs@gmail.com>
2021-01-19 17:20:03 -0300

    feat: Remoção do component CardTask e inserção do novo estilo do accordion

--- src/containers/ContainerPrincipal/Todo/index.jsx
+++ src/containers/ContainerPrincipal/Todo/index.jsx
@@ -1,16 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import useStyles from './styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import { Container } from '@material-ui/core';
-import useStyles from './styles';
-import CardTask from '../../../components/CardTask';
 import TodoForm from './../TodoForm/index';
-import moment from 'moment';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import InfoIcon from '@material-ui/icons/Info';
+export default function Todo ( { todos, removeTodo, updateTodo } ) {
+    const styles = useStyles();
+    const [ expanded, setExpanded ] = useState( false );
+    const handleChange = ( panel ) => ( event, isExpanded ) => {
+        setExpanded( isExpanded ? panel : false );
+    };
 
-function Todo ( { todos, removeTodo, updateTodo } ) {
-
-    const styles = useStyles()
     const [ edit, setEdit ] = useState( {
         id: null,
         value: '',
@@ -35,35 +42,49 @@ function Todo ( { todos, removeTodo, updateTodo } ) {
         />
     }
     return (
-
         todos.map( ( todo, index ) => (
             <Container className={ styles.listaBody }
-                key={ index }
             >
-                <CardTask
-                    className={ styles.cardListado }
-                    id={ todo.id }
-                    title={ todo.title }
-                    desc={ todo.desc }
-                    date={ moment( todo.date ).format( 'DD/MM/YYYY' ) }
-                    favorite={
-                        todo.isFavorite ? <InfoIcon /> : null
-                    }
-                    remove={
-                        <DeleteIcon
-                            className={ styles.icons }
-                            onClick={ () => removeTodo( todo.id ) }
+                <Accordion className={ styles.acordionPrincipal }>
+                    <AccordionSummary
+                        expandIcon={ <ExpandMoreIcon /> }
+                        aria-label="Expand"
+                        aria-controls="additional-actions1-content"
+                        id="additional-actions1-header"
+                    >
+                        <FormControlLabel
+                            aria-label="Acknowledge"
+                            onClick={ ( event ) => event.stopPropagation() }
+                            onFocus={ ( event ) => event.stopPropagation() }
+                            control={ <InfoIcon
+                                onClick={ () => removeTodo( todo.id ) }
+                            /> }
+                            label=""
                         />
-                    }
-                    edit={
-                        <EditIcon
-                            className={ styles.icons }
-                            onClick={ () => setEdit( { id: todo.id, value: todo.title, desc: todo.desc, date: todo.date } ) }
-                        />
-                    }
-                />
+
+                        <Container className={ styles.info }>
+                            <Typography>{ todo.title }</Typography>
+                            <Typography >{ todo.date }</Typography>
+                        </Container>
+                        <Container className={ styles.icons }>
+                            <DeleteIcon
+                                onClick={ () => removeTodo( todo.id ) }
+                            />
+                            <EditIcon
+                                className={ styles.icons }
+                                onClick={ () => setEdit( { id: todo.id, value: todo.title, desc: todo.desc, date: todo.date } ) }
+                            />
+                        </Container>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            { todo.desc }
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+
+
             </Container>
         ) )
-    )
+    );
 }
-export default Todo
